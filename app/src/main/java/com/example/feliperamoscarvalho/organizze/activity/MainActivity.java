4117c8c75576f876b934565b1f96d0b238553d13
Felipe <ramos.felippe@gmail.com>
2018-10-24 11:24:50 -0300

    -Telas de cadastro e de Login -Versao inicial do metodo de cadastro

--- app/src/main/java/com/example/feliperamoscarvalho/organizze/activity/MainActivity.java
+++ app/src/main/java/com/example/feliperamoscarvalho/organizze/activity/MainActivity.java
@@ -1,8 +1,13 @@
-package com.example.feliperamoscarvalho.organizze;
+package com.example.feliperamoscarvalho.organizze.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.feliperamoscarvalho.organizze.R;
+import com.example.feliperamoscarvalho.organizze.activity.CadastroActivity;
+import com.example.feliperamoscarvalho.organizze.activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
@@ -36,5 +41,17 @@ public class MainActivity extends IntroActivity {
                 .fragment(R.layout.intro_4)
                 .build());
 
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_blue_light)
+                .fragment(R.layout.intro_cadastro)
+                .build());
+    }
+
+    public void btEntrar(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void btCadastrar(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
     }
 }
---
+++ app/src/main/java/com/example/feliperamoscarvalho/organizze/config/ConfiguracaoFirebase.java
@@ -0,0 +1,17 @@
+package com.example.feliperamoscarvalho.organizze.config;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ConfiguracaoFirebase {
+
+    private static FirebaseAuth autenticacao;
+
+    //Retorna a instância do FirebaseAuth
+    public static FirebaseAuth getFirebaseAutenticacao(){
+        if(autenticacao == null){
+            autenticacao = FirebaseAuth.getInstance();
+        }
+        return autenticacao;
+    }
+
+}
